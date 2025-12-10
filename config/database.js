f47e52266db31595ce3a34bca7600daa3098172b
Mohammed Lateef <late0024@algonquinlive.com>
2025-12-10 18:00:29 -0500

    fixing experiment

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
-// config/database.js
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: env("DATABASE_CLIENT", "sqlite"),
     connection: {
-      connectionString: env("DATABASE_URL"),
-      ssl: {
-        rejectUnauthorized: false, // needed for Neon
-      },
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "strapi"),
+      ssl: env.bool("DATABASE_SSL", false),
     },
-    pool: { min: 0, max: 10 },
   },
 });
